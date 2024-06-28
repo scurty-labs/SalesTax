aa8998ff4dd32f33b7813f3b75e4f8aff474d1f2
scurty-labs <scurrttie@gmail.com>
2024-06-27 23:59:13 -0700

    verify .diff comparison

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require("express");
-
 const app = express();
-
-app.listen(8080, () =>{
-    console.log("Server is running on http://localhost:8080");
-});
 
 app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
     const options = {root: __dirname};
-    res.sendFile('index.html', options);
+    res.sendFile('Index.Html', options);
 });
+
+app.listen(8080, () =>{
+    console.log("Server is running on http://localhost:8080");
+});
+
+/* -- diff
+const express = require('express');
+const app = express();
+const path = require(path); // Not sure if this was a typo
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(Server listening on port ${PORT}...);
+});
+*/
