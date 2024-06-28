0027d3bcfbdaf470d5f86fcbf1aa47f6137cc5c7
scurty-labs <scurrttie@gmail.com>
2024-06-28 00:24:25 -0700

    do const string for index path

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// directory/files *should* be case sensitive to avoid fuckery
+const index_path = "index.html"
+
 // basically json/post middleware I.E: express.json(); Nifty indeed...
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname));
@@ -8,11 +11,10 @@ app.use(express.static(__dirname));
 app.get('/', function (req, res) {
     const options = {root: __dirname};
 
-    // directory/files are case sensitive
-    res.sendFile('index.html', options);
+    res.sendFile(index_path, options);
 });
 
-// As described in diff
+// As described in diff/google documentation
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log("Server is running on http://localhost:8080");
